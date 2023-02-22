5b41aa073c6d678b4669372d6013e2190a23d681
Andrea Brunelli <andrea.brunelli@24consulting.it>
2023-02-22 16:28:43 +0100

    minor changes

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ import pandas as pd
 import streamlit as st
 
 from functions import dict_to_df, analyze_general_documents
+
+st.set_page_config(page_title='Just Table Recognizer', page_icon = "fiore-del-centro-benessere", initial_sidebar_state = 'auto')
+
+
 st.image("https://upload.wikimedia.org/wikipedia/commons/f/f0/Logo_Just.svg", width=150)
 st.title('Just Table Recognizer')
 
@@ -20,5 +24,11 @@ if image:
     # convert the received model to a dictionary
     analyze_result_dict = result.to_dict()
     df = dict_to_df(analyze_result_dict)
+    if analyze_result_dict['documents'][0]['fields']['Cliente']['value']:
+        st.write(f"**Nome Cliente:** {analyze_result_dict['documents'][0]['fields']['Cliente']['value']}")
+    if analyze_result_dict['documents'][0]['fields']['Cliente']['value']:
+        st.write(f"**Telefono:** {analyze_result_dict['documents'][0]['fields']['Telefono']['value']}")
+    if analyze_result_dict['documents'][0]['fields']['Cliente']['value']:
+        st.write(f"**Importo Totale:** {analyze_result_dict['documents'][0]['fields']['Totale']['value']}€")
 
     st.write(df)
