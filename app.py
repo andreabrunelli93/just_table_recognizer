3776f2b188134a76ee2180db867ac81bd527d895
Andrea Brunelli <andrea.brunelli@24consulting.it>
2023-02-22 14:57:46 +0100

    file uploader

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ from azure.core.credentials import AzureKeyCredential
 from azure.core.serialization import AzureJSONEncoder
 
 from functions import format_bounding_region, format_polygon, dict_to_df, analyze_general_documents
-
+st.image("https://upload.wikimedia.org/wikipedia/commons/f/f0/Logo_Just.svg", width=150)
 st.title('Just Table Recognizer')
 
-url = "test_images\-1421603094_20191015_134815_3470632_resized_storto.jpg"
-result = analyze_general_documents(url)
+image = st.file_uploader("Fotografa la tabella desiderata")
 
-# convert the received model to a dictionary
-analyze_result_dict = result.to_dict()
-df = dict_to_df(analyze_result_dict)
-df
+if image:
+    url = "test_images\-1421603094_20191015_134815_3470632_resized_storto.jpg"
+    result = analyze_general_documents(image)
+    # convert the received model to a dictionary
+    analyze_result_dict = result.to_dict()
+    df = dict_to_df(analyze_result_dict)
 
-st.write(df)
+    st.write(df)
